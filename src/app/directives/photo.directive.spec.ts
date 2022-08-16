e052d549df547b4393e994a690d854dbc350f94e
gavintracy <gavintracy80@gmail.com>
2022-08-16 21:50:05 +0100

    fix gitignore

--- src/app/directives/photo.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PhotoDirective } from './photo.directive';
-
-describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new PhotoDirective();
-    expect(directive).toBeTruthy();
-  });
-});
